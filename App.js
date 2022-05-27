b5bcd43597bdd73642d83bb728096df44c63e0a0
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-05-27 09:40:40 -0500

    Moved StackNavigator.js contents to App.js

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './StackNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './screens/Home'
+import Second from './screens/Second'
+import Modal from './screens/Modal'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Group>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name='Second' component={Second} />
+        </Stack.Group>
+        <Stack.Group screenOptions={{presentation: 'modal'}}>
+          <Stack.Screen name='Modal' component={Modal} />
+        </Stack.Group>
+      </Stack.Navigator>
       <StatusBar style='dark' />
     </NavigationContainer>
   );
