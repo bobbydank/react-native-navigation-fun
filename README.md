ba8c537f74b47e090657b7585afbafd789c0047c
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-05-26 22:50:48 -0500

    Experimentation with Stacks

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Native Navigation Fun
+
+Just a playground for me to test and experiment with React-Native and React-Navigation
+
+## Resources used:
+
+https://www.youtube.com/watch?v=3hLQURJM7ws&ab_channel=LogRocket
---
+++ screens/Home.js
@@ -0,0 +1,32 @@
+import { View, Text, Button, StyleSheet } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
+
+const Home = () => {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button 
+        onPress={() => navigation.navigate("Second")} 
+        title="Go to second screen"
+      />
+    </SafeAreaView>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: "center"
+  },
+  text: {
+      textAlign: "center",
+      fontSize: 30,
+      marginBottom: 30
+  }
+})
